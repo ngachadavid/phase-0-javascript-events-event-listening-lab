9e41290eca9b687a0df4c4c65ef1eab688881e98
ngachadavid <davidngacha01@gmail.com>
2022-12-15 17:02:50 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// function addingEventListener() {
+
+//     const input = document.getElementById('input');
+
+//     function clickAlert() {
+//     alert('I was clicked!');
+// }
+
+// input.addEventListener();
+// }
+
+let input;
+
 function addingEventListener() {
+    input = document.getElementById('input');
+    input.addEventListener('click', ()=> alert('I was clicked!'));
+
 }
+console.log(addingEventListener())
+
